03bbe276401308421e2a988209e47105dd2cf707
RoninInPC <62109908+RoninInPC@users.noreply.github.com>
2020-05-14 13:38:50 +0500

    Update main.c

--- Project2/main.c
+++ Project2/main.c
@@ -2,8 +2,7 @@
 #include <stdio.h>
 #include<string.h>
 #include<tlhelp32.h>
-int Find(HANDLE CONST hStdOut,char* s) {
-    int k;
+DWORD Find(HANDLE CONST hStdOut,char* s) {
     PROCESSENTRY32 peProcessEntry;
     TCHAR szBuff[1024];
     DWORD dwTemp;
@@ -11,27 +10,43 @@ int Find(HANDLE CONST hStdOut,char* s) {
     if (INVALID_HANDLE_VALUE == hSnapshot) {
         return;
     }
-
     peProcessEntry.dwSize = sizeof(PROCESSENTRY32);
     Process32First(hSnapshot, &peProcessEntry);
-    int c = 0;
+    int c;
     do {
-        k = 1;
+        c = 1;
         for (int i = 0; i < strlen(s); i++) {
             if ((char)peProcessEntry.szExeFile[i] != s[i]) {
-                k = 0;
+                c = 0;
                 break;
-            }
+            } 
+        } 
+        if (c == 1) {
+            return peProcessEntry.th32ParentProcessID;
         }
-            if (k == 1) {
-                c++;
-        }
+    } while (Process32Next(hSnapshot, &peProcessEntry));
+    CloseHandle(hSnapshot);
+    return 0;
+}
+int FindCount(HANDLE CONST hStdOut, char* s) {
+    DWORD Found = Find(hStdOut, s);
+    int k = 0;
+    PROCESSENTRY32 peProcessEntry;
+    TCHAR szBuff[1024];
+    DWORD dwTemp;
+    HANDLE CONST hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (INVALID_HANDLE_VALUE == hSnapshot) {
+        return;
+    }
+    peProcessEntry.dwSize = sizeof(PROCESSENTRY32);
+    Process32First(hSnapshot, &peProcessEntry);
+    do {
+        if (peProcessEntry.th32ParentProcessID == Found) k++;
     
     } while (Process32Next(hSnapshot, &peProcessEntry));
     CloseHandle(hSnapshot);
-    return c;
+    return k;
 }
-
 int main(int argc, char* argv[]) {
     int c = 0;
     for (int i = 0; i < strlen(argv[0]); i++) {
@@ -50,7 +65,7 @@ int main(int argc, char* argv[]) {
         s[k - j] = argv[0][k];
     }
     HANDLE CONST hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (Find(hStdOut, s) <= 1) {
+    if (FindCount(hStdOut, s) <= 13) {
         printf("At's good\n");
     }
     else
